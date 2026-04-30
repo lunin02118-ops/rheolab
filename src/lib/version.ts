3880ba6aa7fcc822e4ecfe5dad2244d0a6cdccee
70lunin021189-ux <70lunin021189-ux@github.com>
2026-04-30 23:00:47 +0500

    chore(release): bump alpha to 0.2.2-alpha.5

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -6,6 +6,6 @@
  * Run `npm run version:sync` to regenerate this file.
  */
 
-export const APP_VERSION = '0.2.2-alpha.4';
+export const APP_VERSION = '0.2.2-alpha.5';
 export const BUILD_DATE = '2026-04-30';
-export const COMMIT_HASH = '6e14e1d';
+export const COMMIT_HASH = '0940150';
